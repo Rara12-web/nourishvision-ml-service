9f4005c265daa55080cd55605aef0546342397a8
Rara12-web <mutiaraaviva12@gmail.com>
2026-09-10 22:23:03 +0700

    Use Railway PORT

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
 from flask import Flask, request, jsonify
 from io import BytesIO
+import os
 import numpy as np
 from tensorflow.keras.models import load_model
 from tensorflow.keras.preprocessing import image
@@ -56,4 +57,5 @@ def predict():
 
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=5000)
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port)
